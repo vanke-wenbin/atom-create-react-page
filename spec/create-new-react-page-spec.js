7dd4dcdd00addef6ad551fa844aebb85f6e4d0fc
B05FG36.刘文彬.Bin <liuwb06@vanke.net.cn>
2017-11-28 14:19:46 +0800

    start atom package

---
+++ spec/create-new-react-page-spec.js
@@ -0,0 +1,17 @@
+'use babel';
+
+import CreateNewReactPage from '../lib/create-new-react-page';
+
+// Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
+//
+// To run a specific `it` or `describe` block add an `f` to the front (e.g. `fit`
+// or `fdescribe`). Remove the `f` to unfocus the block.
+
+describe('CreateNewReactPage', () => {
+  let workspaceElement, activationPromise;
+
+  beforeEach(() => {
+    workspaceElement = atom.views.getView(atom.workspace);
+    activationPromise = atom.packages.activatePackage('create-new-react-page');
+  });
+});
---
+++ spec/create-new-react-page-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import CreateNewReactPageView from '../lib/create-new-react-page-view';
+
+describe('CreateNewReactPageView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
